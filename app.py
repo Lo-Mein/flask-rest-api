c6e78da97297cf9bf9138cf4c28470c5a18791e9
Ryan Lo <ryan.lo@ibm.com>
2022-11-15 16:13:29 -0500

    trying to load database

--- app.py
+++ app.py
@@ -31,13 +31,16 @@ def create_app(db_url=None):
     app.config["OPENAPI_SWAGGER_UI_URL"] = "https://cdn.jsdelivr.net/npm/swagger-ui-dist/"
     app.config["SQLALCHEMY_DATABASE_URI"] = db_url or os.getenv("DATABASE_URL", "sqlite:///data.db")
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-    
+
     db.init_app(app)
     migrate = Migrate(app, db)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "jwt-secret-string")
     jwt = JWTManager(app)
+
+    with app.app_context():
+        db.create_all()
 
     @jwt.token_in_blocklist_loader
     def check_if_token_in_blocklist(jwt_header, jwt_payload):
