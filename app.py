7002d03a90f287fa0519a139e42dd9d4341b9829
Ryan Lo <ryan.lo@ibm.com>
2022-11-15 21:02:39 -0500

    Add model changes and mgrations, make sure migration run before app starts

--- app.py
+++ app.py
@@ -33,7 +33,7 @@ def create_app(db_url=None):
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
     db.init_app(app)
-    migrate = Migrate(app, db)
+    migrate = Migrate(app, db, compare_type=True)
     api = Api(app)
 
     app.config["JWT_SECRET_KEY"] = os.getenv("JWT_SECRET_KEY", "jwt-secret-string")
